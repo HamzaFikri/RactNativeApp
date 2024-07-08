89d659cf3cb6698eac7de85234a60bf01be5a238
HamzaFikri <mohammed.hamza.fikri@gmail.com>
2024-07-08 01:29:16 +0100

    Update App.js

--- to-do-app/App.js
+++ to-do-app/App.js
@@ -1,18 +1,19 @@
 import React,{ useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-
+import Header from './components/header';
 
 export default function App() {
   const [stats, setStats] = useState([
     { text: 'Task', key: '1'},
     { text: 'Date', key: '2'},
     { text: 'Status', key: '3'},
-    { text: 'Note', key: '4'},
+    { text: 'Notes', key: '4'},
   ])
   return (
     <View style={styles.container}>
-      {/* {header} */}
+      <Header /> 
+      
       <View style={styles.content}>
         {/* form */}
         <View style={styles.list}>
